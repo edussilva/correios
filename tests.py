52d73e808886b78bbebc53e6fed1d39fd64c629c
Eduardo <edussilva@live.com>
2019-06-06 22:07:58 -0300

    refatorando variáveis estáticas

--- tests.py
+++ tests.py
@@ -1,10 +1,14 @@
 import unittest
 import collections
+from unittest import mock
+from unittest.mock import MagicMock
+from io import StringIO
 
 from correios.core import (
     get_url,
     parse_xml,
     get_servicos_list,
+    handle_request,
 )
 
 class TestGetUrl(unittest.TestCase):
@@ -87,6 +91,34 @@ class TestGetServicosList(unittest.TestCase):
             }
         }
         self.assertEqual(get_servicos_list(data), ['foo', 'bar'])
+
+
+# RESPONSE_MOCK_OK = """<?xml version="1.0" encoding="ISO-8859-1" ?>
+# <Servicos>
+#     <cServico>
+#         <Codigo>04510</Codigo>
+#         <Valor>26,20</Valor>
+#         <PrazoEntrega>6</PrazoEntrega>
+#         <ValorSemAdicionais>26,20</ValorSemAdicionais>
+#         <ValorMaoPropria>0,00</ValorMaoPropria>
+#         <ValorAvisoRecebimento>0,00</ValorAvisoRecebimento>
+#         <ValorValorDeclarado>0,00</ValorValorDeclarado>
+#         <EntregaDomiciliar>S</EntregaDomiciliar>
+#         <EntregaSabado>N</EntregaSabado>
+#         <obsFim></obsFim>
+#         <Erro>0</Erro>
+#         <MsgErro></MsgErro>
+#     </cServico>
+# </Servicos>"""
+
+
+# class TestHandleRequest(unittest.TestCase):
+#     def setUp(self):
+#         self.url = 'http://test.com/?foo=bar&uni=duni+te&abra=cadabra'
+
+#     @mock.patch('correios.core.urlopen', return_value=StringIO(RESPONSE_MOCK_OK)) 
+#     def test_response_ok(self, mock_urlopen):
+#         self.assertEqual(handle_request(self.url), RESPONSE_MOCK_OK)
 
 
 if __name__ == '__main__':
